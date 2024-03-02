5f9b2750eafc286cace0ef968bf2df0a69afb72c
Foxtek <153730800+Acidfurs68@users.noreply.github.com>
2024-03-02 12:02:43 +0100

    Update RESET_CHRONO.ino

--- CHRONOMETER/RESET_CHRONO.ino
+++ CHRONOMETER/RESET_CHRONO.ino
@@ -3,10 +3,14 @@
 LedControl lc = LedControl(10, 11, 12, 1);
 
 int resetPin = A2;
+int buzzerPin = A0;
+int ledPin = 13;  // Remplacez par le numéro de broche approprié pour votre LED
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long elapsedTime = 0;
 bool timingStarted = false;
+bool buttonState = HIGH;
+unsigned long debounceDelay = 20;
 
 void setup() {
   lc.shutdown(0, false);
@@ -15,6 +19,8 @@ void setup() {
   displayTime(0, 0, 0, 0);
 
   pinMode(resetPin, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
@@ -27,6 +33,8 @@ void loop() {
 
     previousMillis = currentMillis; // Réinitialiser le chronomètre
     timingStarted = true; // Démarrer le chronométrage
+          // Produire un bip sonore et clignoter la LED deux fois
+    beepAndBlink();
   }
 
   if (timingStarted) {
@@ -40,6 +48,7 @@ void loop() {
   }
 }
 
+
 void displayTime(int hours, int minutes, int seconds, int milliseconds) {
   lc.setDigit(0, 7, (hours / 10) % 10, false); // Dixième d'heure
   lc.setDigit(0, 6, hours % 10, true);         // Heure
@@ -49,4 +58,15 @@ void displayTime(int hours, int minutes, int seconds, int milliseconds) {
   lc.setDigit(0, 2, seconds % 10, true);       // Seconde
   lc.setDigit(0, 1, (milliseconds / 10) % 10, false);// Dixième de milliseconde
   lc.setDigit(0, 0, milliseconds % 10, false); // Milliseconde
+}
+
+void beepAndBlink() {
+  for (int i = 0; i < 2; ++i) {
+    analogWrite(buzzerPin, 128);  // Utilisez une valeur PWM pour produire un son
+    digitalWrite(ledPin, HIGH);  // Allumer la LED
+    delay(100);  // Maintenir le son et l'état allumé pendant 100 ms
+    analogWrite(buzzerPin, 0);    // Arrêter le son
+    digitalWrite(ledPin, LOW);   // Éteindre la LED
+    delay(100);  // Maintenir l'état éteint pendant 100 ms
+  }
 }
