fd6537ff5bbc24c1633b7540571d3ff66797b17d
Foxtek <153730800+Acidfurs68@users.noreply.github.com>
2024-03-06 12:17:01 +0100

    Update RESET_CHRONO.ino

--- EN/CHRONOMETER/RESET_CHRONO.ino
+++ EN/CHRONOMETER/RESET_CHRONO.ino
@@ -4,7 +4,7 @@ LedControl lc = LedControl(10, 11, 12, 1);
 
 int resetPin = A2;
 int buzzerPin = A0;
-int ledPin = 13; 
+int ledPin = 13;
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long elapsedTime = 0;
@@ -26,14 +26,14 @@ void setup() {
 void loop() {
   currentMillis = millis();
 
-  if (digitalRead(resetPin) == LOW) {
-    // Attendre que le bouton soit relâché pour éviter les rebonds
-    delay(50); 
-    while (digitalRead(resetPin) == LOW); // Attente active jusqu'à ce que le bouton soit relâché
+  if (digitalRead(resetPin) == LOW && buttonState == HIGH && (currentMillis - previousMillis) > debounceDelay) {
+    // Wait for the button to be released to avoid bouncing
+    delay(50);
+    while (digitalRead(resetPin) == LOW); // Active waiting until the button is released
 
-    previousMillis = currentMillis; // Réinitialiser le chronomètre
-    timingStarted = true; // Démarrer le chronométrage
-          // Produire un bip sonore et clignoter la LED deux fois
+    previousMillis = currentMillis; // Reset the timer
+    timingStarted = true; // Start the timer
+    // Produce a beep sound and blink the LED twice
     beepAndBlink();
   }
 
@@ -48,25 +48,24 @@ void loop() {
   }
 }
 
-
 void displayTime(int hours, int minutes, int seconds, int milliseconds) {
-  lc.setDigit(0, 7, (hours / 10) % 10, false); // Dixième d'heure
-  lc.setDigit(0, 6, hours % 10, true);         // Heure
-  lc.setDigit(0, 5, (minutes / 10) % 10, false);// Dixième de minute
+  lc.setDigit(0, 7, (hours / 10) % 10, false); // Tenth of an hour
+  lc.setDigit(0, 6, hours % 10, true);         // Hour
+  lc.setDigit(0, 5, (minutes / 10) % 10, false);// Tenth of a minute
   lc.setDigit(0, 4, minutes % 10, true);        // Minute
-  lc.setDigit(0, 3, (seconds / 10) % 10, false);// Dixième de seconde
-  lc.setDigit(0, 2, seconds % 10, true);       // Seconde
-  lc.setDigit(0, 1, (milliseconds / 10) % 10, false);// Dixième de milliseconde
-  lc.setDigit(0, 0, milliseconds % 10, false); // Milliseconde
+  lc.setDigit(0, 3, (seconds / 10) % 10, false);// Tenth of a second
+  lc.setDigit(0, 2, seconds % 10, true);       // Second
+  lc.setDigit(0, 1, (milliseconds / 10) % 10, false);// Tenth of a millisecond
+  lc.setDigit(0, 0, milliseconds % 10, false); // Millisecond
 }
 
 void beepAndBlink() {
   for (int i = 0; i < 2; ++i) {
-    analogWrite(buzzerPin, 128);  // Utilisez une valeur PWM pour produire un son
-    digitalWrite(ledPin, HIGH);  // Allumer la LED
-    delay(100);  // Maintenir le son et l'état allumé pendant 100 ms
-    analogWrite(buzzerPin, 0);    // Arrêter le son
-    digitalWrite(ledPin, LOW);   // Éteindre la LED
-    delay(100);  // Maintenir l'état éteint pendant 100 ms
+    analogWrite(buzzerPin, 128);  // Use PWM value to produce sound
+    digitalWrite(ledPin, HIGH);  // Turn on the LED
+    delay(100);  // Maintain sound and on state for 100 ms
+    analogWrite(buzzerPin, 0);    // Stop the sound
+    digitalWrite(ledPin, LOW);   // Turn off the LED
+    delay(100);  // Maintain off state for 100 ms
   }
 }
