e3a1aafe1c6a31b7b6127fa9d62b089a748ea7f4
Foxtek <153730800+Acidfurs68@users.noreply.github.com>
2024-03-06 03:12:35 +0100

    Update STOP_RESET_CHRONO.ino

--- CHRONOMETER/STOP_RESET_CHRONO.ino
+++ CHRONOMETER/STOP_RESET_CHRONO.ino
@@ -1,6 +1,8 @@
 #include <LedControl.h>
 
 LedControl lc = LedControl(10, 11, 12, 1);
+const int ledPin = 13;  // Broche pour la LED
+const int buzzerPin = A0;  // Broche pour le buzzer
 
 int resetPin = A2;
 unsigned long previousMillis = 0;
@@ -15,6 +17,7 @@ void setup() {
   displayTime(0, 0, 0, 0);
 
   pinMode(resetPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
@@ -22,14 +25,13 @@ void loop() {
 
   if (digitalRead(resetPin) == LOW) {
     // Attendre que le bouton soit relâché pour éviter les rebonds
-    delay(50); 
+    delay(50);
     while (digitalRead(resetPin) == LOW); // Attente active jusqu'à ce que le bouton soit relâché
 
     if (timingStarted) {
-      timingStarted = false; // Arrêter le chronométrage
+      pauseTimer();  // Mettre en pause le chronomètre
     } else {
-      previousMillis = currentMillis; // Réinitialiser le chronomètre
-      timingStarted = true; // Démarrer le chronométrage
+      restartTimer();  // Réinitialiser et démarrer le chronomètre
     }
   }
 
@@ -41,6 +43,7 @@ void loop() {
     int milliseconds = elapsedTime % 1000 / 10;
 
     displayTime(hours, minutes, seconds, milliseconds);
+
   }
 }
 
@@ -53,4 +56,24 @@ void displayTime(int hours, int minutes, int seconds, int milliseconds) {
   lc.setDigit(0, 2, seconds % 10, true);       // Seconde
   lc.setDigit(0, 1, (milliseconds / 10) % 10, false);// Dixième de milliseconde
   lc.setDigit(0, 0, milliseconds % 10, false); // Milliseconde
+}
+
+void pauseTimer() {
+  for (int i = 0; i < 2; ++i) {
+    tone(buzzerPin, 1000, 100);  // Biper deux fois
+    digitalWrite(ledPin, HIGH);  // Allumer la LED
+    delay(100);
+    digitalWrite(ledPin, LOW);  // Éteindre la LED
+    delay(100);
+  }
+  timingStarted = false;  // Mettre en pause le chronomètre
+}
+
+void restartTimer() {
+  tone(buzzerPin, 1000, 100);  // Biper une fois
+  digitalWrite(ledPin, HIGH);  // Allumer la LED
+  delay(150);
+  digitalWrite(ledPin, LOW);  // Éteindre la LED
+  timingStarted = true;  // Démarrer le chronomètre
+  previousMillis = currentMillis;  // Réinitialiser le chronomètre
 }
