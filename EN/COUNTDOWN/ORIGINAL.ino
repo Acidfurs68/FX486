6d49682b7e14dafeeca299a8b1e719e966cbd553
Foxtek <153730800+Acidfurs68@users.noreply.github.com>
2024-03-06 06:18:12 +0100

    Add files via upload

---
+++ EN/COUNTDOWN/ORIGINAL.ino
@@ -0,0 +1,212 @@
+#include <Keypad.h>
+#include <LedControl.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = {6, 7, 8, 9}; 
+byte colPins[COLS] = {2, 3, 4, 5}; 
+
+Keypad keypadObj = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+LedControl lc = LedControl(10, 11, 12, 1);
+
+long csecondsLeft;
+int decrement = 3;
+bool explosed = false;
+bool defused = false;
+
+long hours = 0;
+long minutes = 0;
+long seconds = 0;
+long hundredths = 0;
+
+String code = "";
+
+char keypadOutput[32] = {0};
+byte playSound = A0;
+
+
+void setup() {
+
+  csecondsLeft = 0;
+  Serial.begin(19200);
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 10);
+  lc.clearDisplay(0); 
+
+  tone(playSound, 3236, 150); delay(300);
+  tone(playSound, 3236, 150); delay(300);
+  tone(playSound, 3236, 150); delay(300);
+  
+  lc.setChar(0,7,'-',false); delay(100);
+  lc.setChar(0,6,'-',false); delay(100);
+  lc.setChar(0,5,'-',false); delay(100);
+  lc.setChar(0,4,'-',false); delay(100);
+  lc.setChar(0,3,'-',false); delay(100);
+  lc.setChar(0,2,'-',false); delay(100);
+  lc.setChar(0,1,'-',false); delay(100);
+  lc.setChar(0,0,'-',false); delay(100);
+
+  lc.clearDisplay(0);
+
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(13, OUTPUT);
+  
+  analogWrite(A0, 0);
+}
+
+bool isProgrammingTime = false;
+String inputKeys = ""; 
+String programmedTime = ""; 
+bool isCountingDown = false; 
+
+void updateDisplay() {
+
+  long remainingSeconds = csecondsLeft / 60;
+
+  hours = remainingSeconds / 3600;
+  remainingSeconds %= 3600;
+  minutes = remainingSeconds / 60;
+  seconds = remainingSeconds % 60;
+  hundredths = csecondsLeft % 60;
+
+  lc.setDigit(0, 7, hours / 10, false);
+  lc.setDigit(0, 6, hours % 10, false);
+  lc.setDigit(0, 5, minutes / 10, false); 
+  lc.setDigit(0, 4, minutes % 10, false);
+  lc.setDigit(0, 3, seconds / 10, false);
+  lc.setDigit(0, 2, seconds % 10, false);
+  lc.setDigit(0, 1, hundredths / 10, false);
+  lc.setDigit(0, 0, hundredths % 10, false);
+}
+
+void programTime() {
+
+  if (inputKeys.length() >= 8) {
+
+    inputKeys = inputKeys.substring(0, 8);
+
+    programmedTime = inputKeys.substring(0, 2) + ":" + inputKeys.substring(2, 4) + ":" + inputKeys.substring(4, 6) + ":" + inputKeys.substring(6, 8);
+
+    Serial.print("inputKeys: "); Serial.println(inputKeys);
+    Serial.print("programmedTime: "); Serial.println(programmedTime);
+
+    lc.clearDisplay(0);
+
+    inputKeys = "";
+  }
+}
+
+void startCountdown() {
+
+  Serial.println("Starting countdown...");
+
+  if (programmedTime.length() == 11) { 
+    hours = programmedTime.substring(0, 2).toInt();
+    minutes = programmedTime.substring(3, 5).toInt();
+    seconds = programmedTime.substring(6, 8).toInt();
+    hundredths = programmedTime.substring(9, 11).toInt();
+
+    csecondsLeft = ((hours * 3600) + (minutes * 60) + seconds) * 60 + hundredths;
+  } else {
+    Serial.println("Invalid programmed time. Please enter a time greater than zero.");
+    return;
+  }
+ 
+  updateDisplay();
+  isCountingDown = true; 
+}
+
+
+void loop() {
+
+  char key = keypadObj.getKey();
+
+  if (key) {
+    Serial.print("Key Pressed: ");
+    Serial.println(key);
+
+    if (key >= '0' && key <= '9') {
+      int num = key - '0';
+      int displayIndex = 7 - inputKeys.length();
+      lc.setDigit(0, displayIndex, num, false);
+      inputKeys += key;
+      tone(playSound, 1000, 50);
+    }
+
+    if (!isProgrammingTime && key == 'A') { 
+
+      Serial.println("Programming time...");
+      isProgrammingTime = true;
+      inputKeys = ""; 
+      programmedTime = ""; 
+      lc.clearDisplay(0); 
+
+    } else if (isProgrammingTime && key == 'D') { 
+
+      isProgrammingTime = false;
+      programTime();
+      Serial.print("Programmed Time: ");
+      Serial.println(programmedTime);
+      startCountdown();
+      programmedTime = ""; 
+
+    } else if (isCountingDown && key == 'B') {
+
+      Serial.println(code);
+      decrement = 0;
+      defused = true;
+      code = "";    
+
+    } else if (isCountingDown && key == 'D') {
+
+      Serial.println(code);
+      decrement = 3;
+      defused = false;
+      code = ""; 
+
+    } else if (key == 'C') {
+
+      isProgrammingTime = false;
+      lc.setDigit(0, 7, 0, false);
+      lc.clearDisplay(0);
+
+    }
+ }
+
+
+  else if (isCountingDown) {
+
+      delay(0); 
+      csecondsLeft -= decrement;
+
+      if (csecondsLeft < 0) {
+        csecondsLeft = 0;
+        if (!explosed) {
+          digitalWrite(13, HIGH);
+          digitalWrite(A1, HIGH);
+          digitalWrite(A2, HIGH);
+          digitalWrite(A3, HIGH);
+          digitalWrite(A4, HIGH);
+          digitalWrite(A5, HIGH);
+        }
+        explosed = true;
+      }
+      updateDisplay(); 
+      if (csecondsLeft % 60 == 0) {
+        tone(playSound, 3236, 50);
+    }
+    
+  }
+
+}
---
+++ EN/COUNTDOWN/README.md
@@ -0,0 +1,71 @@
+# Minuteur à Compte à Rebours Programmable Arduino
+
+![7be9ab2a-c982-4633-9d86-b7a25706d5da](https://github.com/Acidfurs68/FX486_V1/assets/153730800/561f6355-0b7a-4577-b27d-d51a5cb9047f)
+
+Ce projet consiste en un minuteur à compte à rebours programmable basé sur la plateforme Arduino. 
+Il permet aux utilisateurs de définir une durée de compte à rebours en heures, minutes, secondes et centièmes de seconde, puis de lancer le compte à rebours. 
+Le minuteur affiche ensuite le temps restant sur un afficheur 8 chiffres à LED et émet un bip à chaque seconde de décompte.
+
+## Fonctionnalités
+
+- Programmation facile du temps de compte à rebours.
+- Affichage du temps restant sur un afficheur 8 chiffres à LED.
+- Bips sonores à chaque seconde de décompte.
+- Possibilité d'arrêter et relancer le compte à rebours en marche.
+
+## Matériel Requis
+
+- Arduino (par exemple, Arduino Nano)
+- Afficheur 8 chiffres à LED compatible MAX7219
+- Clavier matriciel 4x4
+- Haut-parleur ou buzzer
+- Composants électroniques de base (résistances, fils, etc.)
+
+## Prérequis
+
+Avant de commencer, assurez-vous d'avoir installé l'environnement de développement Arduino sur votre ordinateur. Vous aurez également besoin des bibliothèques suivantes :
+
+- Bibliothèque Keypad : [https://github.com/Chris--A/Keypad](https://github.com/Chris--A/Keypad)
+- Bibliothèque LedControl : [https://github.com/wayoda/LedControl](https://github.com/wayoda/LedControl)
+
+## Installation
+
+1. Assurez-vous d'avoir l'environnement de développement Arduino installé sur votre ordinateur.
+
+2. Installez les bibliothèques nécessaires : `Keypad` et `LedControl`. Vous pouvez les trouver dans les liens fournis dans le code source.
+
+3. Connectez votre carte Arduino aux composants conformément au schéma de câblage fourni dans la documentation.
+
+4. Chargez le code dans l'Arduino IDE et téléversez-le sur votre carte Arduino.
+
+## Utilisation
+
+### Programmation de la durée
+
+- Un appuie sur la touche "A" permet d'entrer en mode de programmation. Vous pouvez commencer à entrer la durée du compte à rebours.
+
+- Utilisez les touches numériques (0-9) du clavier matriciel pour entrer la durée du compte à rebours au format "HHMMSScc", où HH représente les heures, MM les minutes, SS les secondes et cc les centièmes de seconde.
+
+### Démarrer le compte à rebours
+
+- Appuyez sur la touche "D" pour démarrer le compte à rebours. L'afficheur affichera la durée restante.
+
+- Le minuteur émettra un bip sonore à chaque seconde de décompte.
+
+### Arrêter ou relancer le compte à rebours
+
+- Appuyez sur la touche "B" pour arrêter le compte à rebours en cours. Le minuteur s'arrêtera, mais la durée programmée restera affichée.
+
+- Appuyez sur la touche "D" pour relancer le compte à rebours en cours. 
+
+### Réinitialiser et reprogrammer
+
+- En mode programmation, pour effacer la durée programmée et recommencer, appuyez sur la touche "C", et vous pourrez recommencer le processus de programmation en appuyant sur la touche "A".
+
+## Contributions
+
+Si vous souhaitez contribuer à ce projet, suivez les étapes de contribution mentionnées dans le README. Assurez-vous de respecter les normes de codage et les directives de contribution.
+
+## Licence
+
+Ce projet est sous licence XYZ. Pour plus de détails, consultez le fichier `LICENSE.md`.
